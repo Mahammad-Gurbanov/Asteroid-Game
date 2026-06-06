41ebf9b87ae244ae496a32d0e4ba2c363f8e363e
MahammadGurbanov <mahammadgurbanovbusiness@gmail.com>
2026-06-06 22:23:12 +0400

    add circleshape

---
+++ circleshape.py
@@ -0,0 +1,22 @@
+# Retrieved from https://www.boot.dev/lessons/a56fface-8a77-40ee-bf17-50d76078e5ec
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    containers: tuple[pygame.sprite.Group, ...]
+
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        if hasattr(self, "containers"):
+            super().__init__(*self.containers)
+        else:
+            super().__init__()
+
+        self.position: pygame.Vector2 = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pass
+
+    def update(self, dt: float) -> None:
+        pass
